90daeeb384113b19d123bc0982705fa4bf06179b
RogerioPHJ <44878115+RogerioPHJ@users.noreply.github.com>
2020-01-24 09:35:45 -0300

    Meu primeiro Commit

---
+++ FS.TesteASPNET.Web/Controllers/TesteController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FS.TesteASPNET.Web.Models;
+
+namespace FS.TesteASPNET.Web.Controllers
+{
+    public class TesteController : Controller
+    {
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Index([Bind(Include = "Nome,Idade,Endereco,Email")]Pessoa pessoa)
+        {
+            // string nome = formCollection["nome"];
+            /*ViewBag.PessoaInformada = pessoa;
+            return View("Saudacao")*/ //Esses dois foram trocados pela linha abaixo
+            if (ModelState.IsValid) 
+            {
+                pessoa.Ativo = true;
+                return View("Saudacao", pessoa);
+            }
+            else
+            {
+                return View(pessoa);
+            }
+        }
+
+    }
+}
---
+++ FS.TesteASPNET.Web/Models/Pessoa.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace FS.TesteASPNET.Web.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [DisplayName("Nome da Pessoa:")]
+        [MaxLength(50, ErrorMessage = "O nome pode ter no max 50 Caracteres")]
+        public string Nome { get; set; }
+
+        [DisplayName("Idade da Pessoa:")]
+        public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O endereço é obrigatório")]
+        [DisplayName("Enderço da Pessoa:")]
+        [MaxLength(100, ErrorMessage = "O endereço pode ter no max 100 Caracteres")]
+        public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [DisplayName("Email da Pessoa:")]
+        [MaxLength(40, ErrorMessage = "O email pode ter no max 40 Caracteres")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+
+        public bool Ativo { get; set; }
+    }
+}
